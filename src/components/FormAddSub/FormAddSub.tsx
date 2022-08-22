564db5a6f967a47ffe5fb947777d151bd61c32cb
Víctor Tamayo <victortamayo509@gmail.com>
2022-08-22 18:18:20 -0500

    form add sub

--- src/components/FormAddSub/FormAddSub.tsx
+++ src/components/FormAddSub/FormAddSub.tsx
@@ -1,14 +1,30 @@
+import React, { useState } from "react"
+
 function FormAddSub() {
+
+    const[formData, setFormData]=useState({
+        nick:'',
+        subMonths: 0,
+        avatar: '',
+        description: ''
+    })
 
     const handleSubmit=()=>{}
 
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
-        <div>
+        <div className="formAddSub">
             <form onSubmit={handleSubmit}>
-                <input type="text" name="nick" placeholder="nick"/>
-                <input type="text" name="subMoths" placeholder="subMoths"/>
-                <input type="text" name="avatar" placeholder="avatar"/>
-                <input type="text" name="description" placeholder="description"/>
+                <input type="text" value={formData.nick}  onChange={handleChange}  name="nick" placeholder="nick"/>
+                <input type="number" value={formData.subMonths}  onChange={handleChange}  name="subMoths" placeholder="subMoths"/>
+                <input type="text" value={formData.avatar}  onChange={handleChange}  name="avatar" placeholder="avatar"/>
+                <input type="text" value={formData.description}  onChange={handleChange}  name="description" placeholder="description"/>
 
                 <button>save new Sub!</button>
             </form>
